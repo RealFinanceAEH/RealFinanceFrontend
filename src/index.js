f2201a403f78a71c3f1c59b683231093f29f9d0f
Packneu <131830216+Packneu@users.noreply.github.com>
2025-02-06 17:05:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Импортируем createRoot
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // Используем createRoot
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
